5c9c4c8a1a98965f2d6b74de44f728e013acfbaf
namansharma20 <sharmanaman9981@gmail.com>
2023-08-09 17:24:25 +0530

    Added first component using bootstrap.

---
+++ src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<template>
+    <NavBar />
+    <InputForm />
+</template>
+
+
+<script>
+import NavBar from '../components/NavBar.vue';
+import InputForm from '../components/InputForm.vue'
+
+export default {
+    name:'HomeView',
+    components: { NavBar,InputForm}
+}
+</script>
+
